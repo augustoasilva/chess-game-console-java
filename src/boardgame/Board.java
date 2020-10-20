cb8c7e0e77d68142cc8c83a4b81e01e84a6b678b
Augusto Silva <augusto.a.silva@hotmail.com>
2020-10-20 12:29:00 -0300

    Switched from Eclipse to Netbeans

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,44 +1,44 @@
 package boardgame;
 
 public class Board {
-	
-	private int rows;
-	private int columns;
-	private Piece[][] pieces;
-	
-	public Board(int rows, int columns) {
-		this.rows = rows;
-		this.columns = columns;
-		pieces = new Piece[rows][columns];
-	}
 
-	public int getRows() {
-		return rows;
-	}
+    private int rows;
+    private int columns;
+    private final Piece[][] pieces;
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
 
-	public int getColumns() {
-		return columns;
-	}
+    public int getRows() {
+        return rows;
+    }
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
-	public Piece piece(int row, int column) {
-		return pieces[row][column];
-	}
-	
-	public Piece piece(Position position) {
-		return pieces[position.getRow()][position.getColumn()];
-	}
-	
-	public void placePiece(Piece piece, Position position) {
-		pieces[position.getRow()][position.getColumn()] = piece;
-		piece.position = position;
-	}
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 
 }
